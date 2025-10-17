79edf8f410de410ccbdbbc6ba45e5f98087b076e
Amit Maisuria <maisuria_amit@yahoo.com>
2025-10-17 02:35:35 -0400

    payload cms

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
